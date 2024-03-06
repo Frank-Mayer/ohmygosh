84d956dd55a21ded7d659427c5d1169c97db827e
Frank Mayer <gh@frankmayer.dev>
2024-03-06 21:31:49 +0100

    fix(execute): better error messages

--- compiler/execute_default_unix.go
+++ compiler/execute_default_unix.go
@@ -31,24 +31,29 @@ func (c *Command) execute_default() error {
 
 	err := cmd.Run()
 	if err != nil {
-		fmt.Fprintf(**c.Stderr, "failed to execute command: %s\n", c.String())
-		fmt.Fprintln(**c.Stderr, err.Error())
+		fmt.Fprintf(**c.Stderr, "%s: failed to execute command: %s\n", filepath.Base(cmd.Path), err)
 		return errors.Join(fmt.Errorf("failed to execute command %q", c.String()), err)
 	}
 	return nil
 }
 
 func (c *Command) execute_sudo() error {
+	sudoPath, err := exec.LookPath("sudo")
+	if err != nil {
+		fmt.Fprintf(**c.Stderr, "sudo: %s\n", err)
+		return errors.Join(fmt.Errorf("failed to execute command %q", c.String()), err)
+	}
 	cmd := &exec.Cmd{
 		Stdin:  **c.Stdin,
 		Stdout: **c.Stdout,
 		Stderr: **c.Stderr,
-		Path:   "sudo",
+		Path:   sudoPath,
 		Args:   append([]string{"sudo", c.Executable}, c.Arguments...),
 	}
 
-	err := cmd.Run()
+	err = cmd.Run()
 	if err != nil {
+		fmt.Fprintf(**c.Stderr, "sudo: failed to execute command: %s\n", err)
 		return errors.Join(fmt.Errorf("failed to execute command %q", c.String()), err)
 	}
 	return nil
--- compiler/execute_default_windows.go
+++ compiler/execute_default_windows.go
@@ -19,50 +19,69 @@ func (c *Command) execute_default() error {
 	if exe, err := exec.LookPath(c.Executable); err == nil {
 		cmd.Path = exe
 		cmd.Args = append([]string{exe}, c.Arguments...)
+	} else if exe, err := filepath.Abs(c.Executable); err == nil && exists(exe) {
+		cmd.Path = exe
+		cmd.Args = append([]string{exe}, c.Arguments...)
+	} else if pwsh, err := exec.LookPath("pwsh"); err == nil {
+		// use pwsh as a fallback (PowerShell Core)
+		cmd.Path = pwsh
+		cmd.Args = make([]string, 0, len(c.Arguments)+3)
+		cmd.Args = append(cmd.Args, pwsh)
+		cmd.Args = append(cmd.Args, "-Command")
+		cmd.Args = append(cmd.Args, c.Executable)
+		cmd.Args = append(cmd.Args, c.Arguments...)
+	} else if powershell, err := exec.LookPath("powershell"); err == nil {
+		// use powershell as a fallback
+		cmd.Path = powershell
+		cmd.Args = make([]string, 0, len(c.Arguments)+3)
+		cmd.Args = append(cmd.Args, powershell)
+		cmd.Args = append(cmd.Args, "-Command")
+		cmd.Args = append(cmd.Args, c.Executable)
+		cmd.Args = append(cmd.Args, c.Arguments...)
+	} else if cmdExe, err := exec.LookPath("cmd"); err == nil {
+		// use cmd.exe as a fallback
+		cmd.Path = cmdExe
+		cmd.Args = make([]string, 0, len(c.Arguments)+3)
+		cmd.Args = append(cmd.Args, cmdExe)
+		cmd.Args = append(cmd.Args, "/C")
+		cmd.Args = append(cmd.Args, c.Executable)
+		cmd.Args = append(cmd.Args, c.Arguments...)
 	} else {
-		if exe, err := filepath.Abs(c.Executable); err == nil {
-			cmd.Path = exe
-			cmd.Args = append([]string{exe}, c.Arguments...)
-		} else {
-			if pwsh, err := exec.LookPath("pwsh"); err == nil {
-				// use pwsh as a fallback (PowerShell Core)
-				cmd.Path = pwsh
-				cmd.Args = make([]string, 0, len(c.Arguments)+3)
-				cmd.Args = append(cmd.Args, pwsh)
-				cmd.Args = append(cmd.Args, "-Command")
-				cmd.Args = append(cmd.Args, c.Executable)
-				cmd.Args = append(cmd.Args, c.Arguments...)
-			} else if powershell, err := exec.LookPath("powershell"); err == nil {
-				// use powershell as a fallback
-				cmd.Path = powershell
-				cmd.Args = make([]string, 0, len(c.Arguments)+3)
-				cmd.Args = append(cmd.Args, powershell)
-				cmd.Args = append(cmd.Args, "-Command")
-				cmd.Args = append(cmd.Args, c.Executable)
-				cmd.Args = append(cmd.Args, c.Arguments...)
-			} else if cmdExe, err := exec.LookPath("cmd"); err == nil {
-				// use cmd.exe as a fallback
-				cmd.Path = cmdExe
-				cmd.Args = make([]string, 0, len(c.Arguments)+3)
-				cmd.Args = append(cmd.Args, cmdExe)
-				cmd.Args = append(cmd.Args, "/C")
-				cmd.Args = append(cmd.Args, c.Executable)
-				cmd.Args = append(cmd.Args, c.Arguments...)
-			} else {
-				return fmt.Errorf("failed to execute command %q", c.String())
-			}
-		}
+		return fmt.Errorf("failed to execute command %q", c.String())
 	}
 
 	err := cmd.Run()
 	if err != nil {
-		fmt.Fprintf(**c.Stderr, "failed to execute command: %s\n", c.String())
-		fmt.Fprintln(**c.Stderr, err.Error())
+		fmt.Fprintf(**c.Stderr, "%s: failed to execute command: %s\n", filepath.Base(cmd.Path), err)
 		return errors.Join(fmt.Errorf("failed to execute command %q", c.String()), err)
 	}
 	return nil
 }
 
 func (c *Command) execute_sudo() error {
-	return fmt.Errorf("sudo is not supported on Windows")
+	sudoPath, err := exec.LookPath("sudo")
+	if err != nil {
+		fmt.Fprintf(**c.Stderr, "sudo: %s\n", err)
+		fmt.Fprintln(**c.Stderr, "install sudo for Windows from https://github.com/microsoft/sudo")
+		return errors.Join(fmt.Errorf("failed to execute command %q", c.String()), err)
+	}
+	cmd := &exec.Cmd{
+		Stdin:  **c.Stdin,
+		Stdout: **c.Stdout,
+		Stderr: **c.Stderr,
+		Path:   sudoPath,
+		Args:   append([]string{"sudo", c.Executable}, c.Arguments...),
+	}
+
+	err = cmd.Run()
+	if err != nil {
+		fmt.Fprintf(**c.Stderr, "sudo: failed to execute command: %s\n", err)
+		return errors.Join(fmt.Errorf("failed to execute command %q", c.String()), err)
+	}
+	return nil
+}
+
+func exists(path string) bool {
+	_, err := exec.LookPath(path)
+	return err == nil
 }
